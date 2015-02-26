0a53f23da6c3a71f5cc3ecf12d5c5d33ef83f96c
Arne <schoonvlietarne3@gmail.com>
2015-02-26 21:05:06 +0100

    Added new files for database communication with android. We can login now and make new users with encrypted passwords

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "127.7.236.130");
+define("DB_USER", "adminXRSMmKy");
+define("DB_PASSWORD", "qTT65X9F3gSH");
+define("DB_DATABASE", "android_api");
+?>
--- index.php
+++ index.php
@@ -1,2 +1,86 @@
-<?php$host="127.7.236.130"; //replace with database hostname$username="adminXRSMmKy"; //replace with database username$password="qTT65X9F3gSH"; //replace with database password$db_name="php"; //replace with database name//Try to connect to MySQL Server. Give true if it succeeded, false if it failed!$con = mysql_connect("$host", "$username", "$password");if ($con == false)  //Connection failed{	trigger_error("Cannot connect to server");
-}$select = mysql_select_db("$db_name");if ($select == false)  //Connection failed{	trigger_error("Cannot select the database");}$sql = "SELECT * FROM Users";$result = mysql_query($sql);$json = array();if(mysql_num_rows($result)){	while($row=mysql_fetch_assoc($result))	{		$json['users_info'][]=$row;	}}mysql_close($con);echo json_encode($json);?>
+<?php
+
+/**
+ * File to handle all API requests
+ * Accepts GET and POST
+ * 
+ * Each request will be identified by TAG
+ * Response will be JSON data
+
+  /**
+ * check for POST request 
+ */
+if (isset($_POST['tag']) && $_POST['tag'] != '') {
+    // get tag
+    $tag = $_POST['tag'];
+
+    // include db handler
+    require_once 'include/DB_Functions.php';
+    $db = new DB_Functions();
+
+    // response Array
+    $response = array("tag" => $tag, "success" => 0, "error" => 0);
+
+    // check for tag type
+    if ($tag == 'login') {
+        // Request type is check Login
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // check for user
+        $user = $db->getUserByEmailAndPassword($email, $password);
+        if ($user != false) {
+            // user found
+            // echo json with success = 1
+            $response["success"] = 1;
+            $response["uid"] = $user["unique_id"];
+            $response["user"]["name"] = $user["name"];
+            $response["user"]["email"] = $user["email"];
+            $response["user"]["created_at"] = $user["created_at"];
+            $response["user"]["updated_at"] = $user["updated_at"];
+            echo json_encode($response);
+        } else {
+            // user not found
+            // echo json with error = 1
+            $response["error"] = 1;
+            $response["error_msg"] = "Incorrect email or password!";
+            echo json_encode($response);
+        }
+    } else if ($tag == 'register') {
+        // Request type is Register new user
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // check if user is already existed
+        if ($db->isUserExisted($email)) {
+            // user is already existed - error response
+            $response["error"] = 2;
+            $response["error_msg"] = "User already existed";
+            echo json_encode($response);
+        } else {
+            // store user
+            $user = $db->storeUser($name, $email, $password);
+            if ($user) {
+                // user stored successfully
+                $response["success"] = 1;
+                $response["uid"] = $user["unique_id"];
+                $response["user"]["name"] = $user["name"];
+                $response["user"]["email"] = $user["email"];
+                $response["user"]["created_at"] = $user["created_at"];
+                $response["user"]["updated_at"] = $user["updated_at"];
+                echo json_encode($response);
+            } else {
+                // user failed to store
+                $response["error"] = 1;
+                $response["error_msg"] = "Error occured in Registartion";
+                echo json_encode($response);
+            }
+        }
+    } else {
+        echo "Invalid Request";
+    }
+} else {
+    echo "Access Denied";
+}
+?>
